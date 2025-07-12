9b7be8b0f613fb7e7ae4292219b4f0b795b34aff
KAWAKAMI Moeki <hi@moeki.org>
2025-07-13 08:15:58 +0900

    環境変数をやめる

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: `https://${import.meta.env.VITE_FIREBASE_PROJECT_ID}-default-rtdb.asia-southeast1.firebasedatabase.app/`
+  apiKey: "AIzaSyB_z4gypbs7LpYPY2fABwJRoPOpbyF1lx8",
+  authDomain: "maplap-41b08.firebaseapp.com",
+  projectId: "maplap-41b08",
+  storageBucket: "maplap-41b08.firebasestorage.app",
+  messagingSenderId: "618143200763",
+  appId: "1:618143200763:web:c4999dbf7d1dcbb01d475e",
+  databaseURL: "https://maplap-41b08-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
