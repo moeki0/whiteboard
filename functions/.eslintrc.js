ab3c0f625cf1e813cc8c66cc18138ba9b07fdf76
KAWAKAMI Moeki <hi@moeki.org>
2025-07-22 21:07:13 +0900

    Fix

---
+++ functions/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "/generated/**/*", // Ignore generated files.
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    // メインプロジェクトと同じルール
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-undef": "off",
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "import/no-unresolved": 0,
+  },
+};
